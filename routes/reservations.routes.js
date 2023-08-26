09033d42d3ef481d4912fd31d74307802f0de67f
Andres Franco <andresfranla@gmail.com>
2023-08-26 10:17:48 -0600

    reservation routes done, missing middlewares

--- routes/reservations.routes.js
+++ routes/reservations.routes.js
@@ -8,5 +8,26 @@ const {
     deleteResevation
 } = require("../controllers/reservations.controller")
 
-// Verify the user exist on DB
-/* const { isUser } = require('../middleware/isUser.middleware') */
+// Create a available Reservation and add it to the Admin
+router.post("/user/:adminId", createReservation)
+
+
+// Get all the reservations from the Admin
+router.get("/user/:userId", getAllReservations)
+
+// Update Reservation from Admin list (Before 24 hours)
+router.put("/:reservationId/admin/:adminId/user/:userId",
+    assingReservation,
+)
+
+// Remove reservation from Admin list by user (Only 24 hour)
+router.patch("/:reservationId/admin/:adminId/user/:userId",
+    patchRemoveUser
+)
+
+// Delete reservation from Admin reservation list (Only not booked before 24 hour)
+router.delete("/:reservationId/admin/:adminId",
+    deleteResevation
+)
+
+module.exports = router
