4a3128d0017c91ee9f5aab11f0a7935e07f6c99f
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-09-04 22:09:54 -0600

    fixing the routes for the calendar

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const servicesRouter = require('./services.routes')
 const reservationsRouter = require('./reservations.routes')
+const calendarRouter = require('./calendar.routes.js')
 
 router.get("/", (req, res, next) => {
   res.status(200).json({ message: "Welcome to unibook pro API"});
 });
 
-router.use('/servicios', servicesRouter)
+router.use('/servicios', servicesRouter);
 router.use('/reservar-ahora', reservationsRouter);
+router.use('/calendario', calendarRouter);
 
 module.exports = router;
