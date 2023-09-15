823820176bf54e509878d55f7755ab6fd78284d5
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-09-15 16:09:34 -0600

    revert all the changes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,9 +5,6 @@ const reservationsRouter = require('./reservations.routes')
 const calendarRouter = require('./calendar.routes.js')
 const { isAuthenticated } = require("../middleware/jwt.middleware"); 
 const { isAdmin } = require("../middleware/isAdmin.middleware");
-const cors = require('cors')
-
-router.use(cors)
 
 router.get("/", (req, res, next) => {
   res.status(200).json({ message: "Welcome to unibook pro API"});
