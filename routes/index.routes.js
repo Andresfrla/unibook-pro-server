32276fb7572395e56a6e91d6d61743f32e6f0e5e
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-09-15 16:05:14 -0600

    try to fix cors

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,9 +5,11 @@ const reservationsRouter = require('./reservations.routes')
 const calendarRouter = require('./calendar.routes.js')
 const { isAuthenticated } = require("../middleware/jwt.middleware"); 
 const { isAdmin } = require("../middleware/isAdmin.middleware");
+const cors = require('cors')
+
+router.use(cors)
 
 router.get("/", (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
   res.status(200).json({ message: "Welcome to unibook pro API"});
 });
 
